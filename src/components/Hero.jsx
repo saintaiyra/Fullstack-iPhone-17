e597c6ccaeba265d1e05aaf337ccdad3806c59e3
Saint Aiyra <bielcostaexemplo@gmail.com>
2026-09-25 02:09:49 -0300

    backup hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,57 @@
+function Hero() {
+    return (
+        <section className="relative h-screen overflow-hidden bg-black">
 
+            <div className="absolute top-16 bottom-0 left-0 right-0 z-0">
+                <img
+                    src="/img/hero.jpg"
+                    alt="iPhone 17"
+                    className="
+                        w-full
+                        h-full
+                        object-contain
+                        object-center
+                        opacity-90
+                        md:object-cover
+                    "
+                />
 
-    function Hero() {
-        return (
-            <section className="relative h-screen overflow-hidden bg-black">
-                <div className="absolute top-16 bottom-0 right-0 left-0 z-0">
-                    <img src="/img/hero.jpg" className=" w-full h-full object-cover opacity-90"></img>
-                    <div> className="absolute insert-0 bg-gradient-to-b from-black/30 vim-transparent to-black/80"</div>
-                </div>
+                <div className="
+                    absolute
+                    inset-0
+                    bg-linear-to-b
+                    from-black/30
+                    via-transparent
+                    to-black/80
+                "></div>
+            </div>
 
-                <div className="absolute bottom-0 left-1/2 animate-bounce">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-</svg>
+            <div className="
+                absolute
+                bottom-6
+                left-1/2
+                -translate-x-1/2
+                animate-bounce
+                z-10
+            ">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2.5"
+                    stroke="currentColor"
+                    className="size-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+                    />
+                </svg>
+            </div>
 
-                </div>
-            </section>
-        )
-    }
-export default Hero
+        </section>
+    );
+}
+
+export default Hero;
